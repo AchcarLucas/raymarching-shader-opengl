cd4db8a7a62ad07f93ea7936b63e80771a480b6e
Lucas Campos <lucas.campos@ambrait.com>
2023-11-22 23:58:58 -0300

    - ray marching com light

--- glsl/ex_02/shader.fs
+++ glsl/ex_02/shader.fs
@@ -36,10 +36,33 @@ void main()
 #define MAX_DIST 100.0
 #define SURF_DIST 0.01
 
+vec3 getNormal(vec3 p);
+
 float getDist(vec3 p);
 float rayMarch(vec3 ro, vec3 rd);
 float drawSphere(vec3 p, vec3 position, float radiuns);
 float drawPlane(vec3 p);
+float getLight(vec3 p);
+
+vec3 getNormal(vec3 p)
+{
+    float d = getDist(p);
+    vec2 e = vec2(0.01, 0.0);
+    vec3 n = vec3(
+                    d - getDist(p - e.xyy),
+                    d - getDist(p - e.yxy),
+                    d - getDist(p - e.yyx));
+    return normalize(n);
+}
+
+float getLight(vec3 p)
+{
+    vec3 p_light = vec3(0, 5, 6);
+    vec3 v_light = normalize(p_light - p);
+    vec3 normal = getNormal(p);
+    float diffuse = dot(v_light, normal);
+    return diffuse;
+}
 
 float drawSphere(vec3 p, vec3 position, float radiuns)
 {
@@ -101,6 +124,9 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
     // ray direction
     vec3 rd = normalize(vec3(uv.x, uv.y, 1));
 
-    color = vec3(rayMarch(ro, rd)) / 20.0;
+    float d = rayMarch(ro, rd);
+
+    color = vec3(getLight(ro + rd * d));
+
     fragColor = vec4(color, 1.0);
 }
