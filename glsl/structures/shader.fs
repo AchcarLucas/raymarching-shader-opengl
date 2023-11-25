7f5a331be0e1ecffa416a8d6c21f7cd613dda677
Lucas Campos <lucas.campos@ambrait.com>
2023-11-24 23:11:34 -0300

    - ajuste shader structures

--- glsl/structures/shader.fs
+++ glsl/structures/shader.fs
@@ -41,17 +41,17 @@ const float PI=3.14159265358979323846;
 const float value=0.0;
 
 float rand(in vec2 p,in float t)
-	{
+{
 	return fract(sin(dot(p+mod(t,1.0),vec2(12.9898,78.2333)))*43758.5453);
-	}
+}
 
 vec2 rotate(vec2 k,float t)
-	{
+{
 	return vec2(cos(t)*k.x-sin(t)*k.y,sin(t)*k.x+cos(t)*k.y);
-	}
+}
 
 float scene1(vec3 p)
-	{
+{
 	float speed=iTime*0.5;
 	float ground=dot(p,vec3(0.0,1.0,0.0))+0.75;
 	float t1=length(abs(mod(p.xyz,2.0)-1.0))-1.35+0.05*cos(PI*p.x*4.0)+0.05*sin(PI*p.z*4.0);	// structure
@@ -63,18 +63,18 @@ float scene1(vec3 p)
 	float hole=length(abs(mod(p.xz,1.0)-0.5))-hole_w;
 	float tube_p=2.0-0.25*sin(PI*p.z*0.5);
 	float tube_v=PI*8.0;
-    float tube_b=tube_p*0.02;
+	float tube_b=tube_p*0.02;
 	float tube_w=tube_b+tube_b*cos(p.x*tube_v)*sin(p.y*tube_v)*cos(p.z*tube_v)+tube_b*sin(PI*p.z+speed*4.0);
 	float tube=length(abs(mod(p.xy,tube_p)-tube_p*0.5))-tube_w;
 	return min(max(min(-t1,max(-hole-t5*0.375,ground+bubble)),t3+t5),tube);
-	}
+}
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
-	{
+{
 	float speed=iTime*0.5;
-    float ground_x=1.5*cos(PI*speed*0.125);
-    float ground_y=4.0-3.0*sin(PI*speed*0.125)+0.125*value;
-    float ground_z=-1.0-speed;
+	float ground_x=1.5*cos(PI*speed*0.125);
+	float ground_y=4.0-3.0*sin(PI*speed*0.125)+0.125*value;
+	float ground_z=-1.0-speed;
 	vec2 position=fragCoord.xy/iResolution.xy;        
 	vec2 p=-1.0+2.0*position;
 	vec3 dir=normalize(vec3(p*vec2(1.625,1.0),0.75));	// screen ratio (x,y) fov (z)
@@ -87,7 +87,7 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
 	for(int i=0;i<ray_n;i++)
 		{
 		float k=scene1(ray+dir*t);
-        if(abs(k)<0.005) break;
+		if(abs(k)<0.005) break;
 		t+=k*0.5;
 		}
 	vec3 hit=ray+dir*t;
@@ -95,6 +95,6 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
 	vec3 n=normalize(vec3(scene1(hit+h.xyy),scene1(hit+h.yxx),scene1(hit+h.yyx)));
 	float c=(n.x+n.y+n.z)*0.1;
 	vec3 color=vec3(c,c,c)-t*0.0625;
-    //color*=0.6+0.4*rand(vec2(t,t),iTime); // noise!
+	//color*=0.6+0.4*rand(vec2(t,t),iTime); // noise!
 	fragColor=vec4(vec3(c+t*0.08,c+t*0.02,c*1.5-t*0.01)+color*color,1.0);
-	}
+}
